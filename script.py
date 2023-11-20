e04e359ce787c0c4e4ad04dc2313233caadea304
Sakir Malek <sakirm@infopercept.com>
2023-11-20 12:56:06 +0530

    Update script.py

--- script.py
+++ script.py
@@ -2,11 +2,11 @@ import pandas as pd
 import json
 
 # Load JSON file
-with open('/var/lib/jenkins/workspace/test/app.json') as f:
+with open('/var/lib/jenkins/workspace/mobsf/app.json') as f:
     data = json.load(f)
 
 # Convert to DataFrame
 df = pd.json_normalize(data)
 
 # Save to Excel
-df.to_excel('/var/lib/jenkins/workspace/test/app.xlsx', index=False)
+df.to_excel('/var/lib/jenkins/workspace/mobsf/app.xlsx', index=False)
